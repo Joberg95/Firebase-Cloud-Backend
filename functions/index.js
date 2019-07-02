67840e554082a33c300e930701e2aca357eb535a
nevroje <joakim.berglind@gmail.com>
2019-07-02 14:30:32 +0200

    moved config to utils folder

--- functions/index.js
+++ functions/index.js
@@ -5,15 +5,15 @@ const app = express();
 
 admin.initializeApp();
 
-const config = {
-  apiKey: "AIzaSyCMW7hFqdxtLav5jPLyF3_FAURuCy38IRQ",
-  authDomain: "socialape-2abc8.firebaseapp.com",
-  databaseURL: "https://socialape-2abc8.firebaseio.com",
-  projectId: "socialape-2abc8",
-  storageBucket: "socialape-2abc8.appspot.com",
-  messagingSenderId: "580979328273",
-  appId: "1:580979328273:web:a9d2b92100311e83"
-};
+// const config = {
+//   apiKey: "AIzaSyCMW7hFqdxtLav5jPLyF3_FAURuCy38IRQ",
+//   authDomain: "socialape-2abc8.firebaseapp.com",
+//   databaseURL: "https://socialape-2abc8.firebaseio.com",
+//   projectId: "socialape-2abc8",
+//   storageBucket: "socialape-2abc8.appspot.com",
+//   messagingSenderId: "580979328273",
+//   appId: "1:580979328273:web:a9d2b92100311e83"
+// };
 
 const firebase = require("firebase");
 firebase.initializeApp();
