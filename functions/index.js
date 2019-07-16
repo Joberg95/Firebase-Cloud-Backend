c3a08f6661cf6a6be82442ec21ce7aaf7f82ab55
nevroje <joakim.berglind@gmail.com>
2019-07-16 15:26:33 +0200

    formatting

--- functions/index.js
+++ functions/index.js
@@ -58,7 +58,7 @@ const FBAuth = (req, res, next) => {
   admin
     .auth()
     .verifyIdToken()
-    .then((decodedToken) => {
+    .then(decodedToken => {
       req.user = decodedToken;
       console.log(decodedToken);
       return db
@@ -67,16 +67,15 @@ const FBAuth = (req, res, next) => {
         .limit(1)
         .get();
     })
-    .then((data) => {
+    .then(data => {
       req.user.handle = data.docs[0].data().handle;
       return next();
     })
-    .catch((err) => {
+    .catch(err => {
       console.error("Error while verifying token", err);
       return res.status(403).json(err);
     });
 };
-
 
 // Post one scream
 app.post("/scream", FBAuth, (req, res) => {
