72410b593297e0bfc0a4a5b5114a335232c64407
nevroje <joakim.berglind@gmail.com>
2019-07-19 14:13:12 +0200

    formatting

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -18,38 +18,38 @@ exports.signup = (req, res) => {
   let token, userId;
   db.doc(`/users/${newUser.handle}`)
     .get()
-    .then((doc) => {
-        if (doc.exists) {
-            return res.status(400).json({ handle: 'this handle is already taken'});
-        } else {
-            return firebase
-            .auth()
-            .createUserWithEmailAndPassword(newUser.email, newUser.password);
-        }
+    .then(doc => {
+      if (doc.exists) {
+        return res.status(400).json({ handle: "this handle is already taken" });
+      } else {
+        return firebase
+          .auth()
+          .createUserWithEmailAndPassword(newUser.email, newUser.password);
+      }
     })
-    .then((data) => {
-        userId = data.user.uid;
-        return data.user.getIdToken();
+    .then(data => {
+      userId = data.user.uid;
+      return data.user.getIdToken();
     })
-    .then((idToken) => {
-        token = idToken;
-        const userCredentials = {
-            handle: newUser.handle,
-            email: newUser.email,
-            createdAt: new Date().toISOString(),
-            userId
-        };
-        return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    .then(idToken => {
+      token = idToken;
+      const userCredentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId
+      };
+      return db.doc(`/users/${newUser.handle}`).set(userCredentials);
     })
     .then(() => {
-        return res.status(201).json({ token });
+      return res.status(201).json({ token });
     })
-    .catch((err) => {
-        console.error(err);
-        if (err.code === 'auth/email-already-in-use') {
-            return res.status(400).json({ email: 'Email is already in use' });
-        } else {
-            return res.status(500).json({ error: err.code });
-        }
+    .catch(err => {
+      console.error(err);
+      if (err.code === "auth/email-already-in-use") {
+        return res.status(400).json({ email: "Email is already in use" });
+      } else {
+        return res.status(500).json({ error: err.code });
+      }
     });
 };
