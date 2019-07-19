671626d143dcf561113e0b668f4b3069cda0955f
nevroje <joakim.berglind@gmail.com>
2019-07-19 14:12:56 +0200

    created user responses

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -14,4 +14,42 @@ exports.signup = (req, res) => {
 
   const { valid, errors } = validateSignupData(newUser);
   if (!valid) return res.status(400).json(errors);
+
+  let token, userId;
+  db.doc(`/users/${newUser.handle}`)
+    .get()
+    .then((doc) => {
+        if (doc.exists) {
+            return res.status(400).json({ handle: 'this handle is already taken'});
+        } else {
+            return firebase
+            .auth()
+            .createUserWithEmailAndPassword(newUser.email, newUser.password);
+        }
+    })
+    .then((data) => {
+        userId = data.user.uid;
+        return data.user.getIdToken();
+    })
+    .then((idToken) => {
+        token = idToken;
+        const userCredentials = {
+            handle: newUser.handle,
+            email: newUser.email,
+            createdAt: new Date().toISOString(),
+            userId
+        };
+        return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+    .then(() => {
+        return res.status(201).json({ token });
+    })
+    .catch((err) => {
+        console.error(err);
+        if (err.code === 'auth/email-already-in-use') {
+            return res.status(400).json({ email: 'Email is already in use' });
+        } else {
+            return res.status(500).json({ error: err.code });
+        }
+    });
 };
